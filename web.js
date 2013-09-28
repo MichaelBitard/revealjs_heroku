f1c26aba7173b70ac081a149a9127456d52bd817
Michaël Bitard <bitard.michael@gmail.com>
2013-09-28 12:56:42 +0200

    init

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  req.forward('index.html');
+  request.forward('index.html');
 });
 
 var port = process.env.PORT || 5000;
